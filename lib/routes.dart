ffa1d612f989f8670ec196b30bdf9aa3556dda51
Mohamed Adel <ov3rcontrol@hotmail.com>
2020-01-23 16:45:45 +0200

    :sparkles: Add Onboard Screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 import './screens/home/home.dart';
+import './screens/onBoard/onBoardScreen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (BuildContext context) => HomeScreen(),
+  "/": (BuildContext context) => OnBoardingScreen(),
+  "/home": (BuildContext context) => HomeScreen(),
 };
---
+++ lib/screens/onBoard/onBoardScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:app/screens/home/home.dart';
+import '../../theme/styles.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({Key key}) : super(key: key);
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomeScreen()),
+    );
+  }
+
+  Widget _buildImage(String assetName) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 40, 10, 8),
+      child: Align(
+        child: Image.asset(
+          'assets/$assetName.png',
+          width: 400.0,
+        ),
+        alignment: Alignment.bottomCenter,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Color(0xffFE5003),
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "PLAY YOUR FAVORITE GAMES",
+          body:
+              "Providing a Completely Unique Gaming Experience for Every Ene in the E-Sports industry",
+          image: _buildImage('fortnite'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "WIN GAMES, EARN POINTS",
+          body:
+              "Compleletly Unqiue Points System, That Rewards Players For Winning Competitive Games",
+          image: _buildImage('Emblem_Challenger'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "STATS TRACKING",
+          body:
+              "Tracking You In Games and Providing You Information to Improve Your Gameplay",
+          image: _buildImage('stats'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Get Started',
+          style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Colors.white70,
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+    );
+  }
+}
